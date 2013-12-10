b6a50b8a2518459fcd2ea4d463c7060a56b2f668
Tyler Hugenberg (hugey) <tylerhugenberg@gmail.com>
2013-12-10 00:48:03 -0500

    Initial Commit

---
+++ PowerHour/HUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HUAppDelegate.h
+//  PowerHour
+//
+//  Created by Hugey on 12/10/13.
+//  Copyright (c) 2013 Hugey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
